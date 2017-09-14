514a981b9981a21b7f4666caebefd2ecfa1d20a3
pablopaho <papatino@sura.com.co>
2017-09-13 20:42:22 -0500

    fist test... pending mock persistence

--- test/co/com/akku/bikes/services/BikesQueriesServicesTest.scala
+++ test/co/com/akku/bikes/services/BikesQueriesServicesTest.scala
@@ -1,5 +1,23 @@
 package co.com.akku.bikes.services
 
-class BikesQueriesServicesTest {
+import org.scalatestplus.play.{ OneAppPerSuite, PlaySpec }
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+class BikesQueriesServicesTest extends PlaySpec with OneAppPerSuite {
+
+  "BikeQueriesServiceTest" should {
+
+    "Return all bikes" in {
+      //arrange
+      val bikesQueriesService = app.injector.instanceOf[BikesQueriesServices]
+
+      //act
+      val bikes = Await.result(bikesQueriesService.bikes(), 1 seconds)
+
+      //assert
+      assert(bikes.head.id === 1)
+    }
+  }
 
 }
