7aed08842e660c684441d4777d31ff75dd2a577c
juan diego estrada <jdestradap@gmail.com>
2018-01-21 14:13:16 -0500

    more info to results

--- app/co/com/cobaik/bikes/json/objects/BikeDetail.scala
+++ app/co/com/cobaik/bikes/json/objects/BikeDetail.scala
@@ -5,6 +5,8 @@ import org.joda.time.DateTime
 
 
 case class BikeDetail(bikeId: Int, brand: String, reference: String, categoryId: Int, price: Int, properties: Map[String, String], imageUrl: String)
-case class BikeSearchDetail(bikeId: Int, brand: String, reference: String, categoryId: Int, price: Int, properties: Map[String, String], imageUrl: String)
+case class BikeSearchDetail(bike_id: Int, brand: String, reference: String, category_description: String,
+                            price: Int, properties: Map[String, String], image_url: String, cobaik_location: CobaikLocation,
+                            description: String)
 case class BikesQuery(location: CobaikLocation, start_date: Option[DateTime], end_date: Option[DateTime])
 
